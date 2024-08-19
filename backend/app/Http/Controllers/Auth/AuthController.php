37946c3162b83e389f1019eac8749a2104e0037c
laananihamza <loenwi03@gmail.com>
2024-08-19 10:18:09 +0100

    fix problem of auth

--- backend/app/Http/Controllers/Auth/AuthController.php
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -6,21 +6,24 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
+        $user = User::where('email', $credentials['email'])->first();
 
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            $token = $user->createToken('private_school')->plainTextToken;
-
-            return response()->json(['user' => $user,'token' => $token]);
-        }
-
+    if (!$user || !Hash::check($credentials['password'], $user->password)) {
         return response()->json(['error' => 'Unauthorized'], 401);
+
+    }
+        $token = $user->createToken('private_school')->plainTextToken;
+
+        return response()->json(['user' => $user,'token' => $token]);
+
     }
     public function logout(Request $request)
     {
