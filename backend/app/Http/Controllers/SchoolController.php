d0929a2bb4fbdeb9ba7eb62c7ea6365765afbc6b
laananihamza <loenwi03@gmail.com>
2024-08-29 11:59:14 +0100

    add controllers to grade, roles, schools and subjects

--- backend/app/Http/Controllers/SchoolController.php
+++ backend/app/Http/Controllers/SchoolController.php
@@ -4,62 +4,79 @@ namespace App\Http\Controllers;
 
 use App\Models\School;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SchoolController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $perPage = $request->input('per_page', 5);
+        $sortColumn = $request->input('sort_column', 'id');
+        $sortDirection = $request->input('sort_direction', 'asc');
+        $schools = School::orderBy($sortColumn, $sortDirection)->paginate($perPage);
+        return response()->json($schools);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+        ]);
+
+        $school = School::create($request->all());
+        return response()->json($school, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param \App\Models\School $school
+     * @return \Illuminate\Http\Response
      */
     public function show(School $school)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(School $school)
-    {
-        //
+        return response()->json($school);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\School $school
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, School $school)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+        ]);
+
+        $school->update($request->all());
+        return response()->json($school);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param \App\Models\School $school
+     * @return \Illuminate\Http\Response
      */
     public function destroy(School $school)
     {
-        //
+        $school->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
--- backend/app/Http/Controllers/SubjectController.php
+++ backend/app/Http/Controllers/SubjectController.php
@@ -4,62 +4,79 @@ namespace App\Http\Controllers;
 
 use App\Models\Subject;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class SubjectController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $perPage = $request->input('per_page', 5);
+        $sortColumn = $request->input('sort_column', 'id');
+        $sortDirection = $request->input('sort_direction', 'asc');
+        $subjects = Subject::orderBy($sortColumn, $sortDirection)->paginate($perPage);
+        return response()->json($subjects);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'coef' => 'required|numeric|min:0',
+        ]);
+
+        $subject = Subject::create($request->all());
+        return response()->json($subject, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param \App\Models\Subject $subject
+     * @return \Illuminate\Http\Response
      */
     public function show(Subject $subject)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Subject $subject)
-    {
-        //
+        return response()->json($subject);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Subject $subject
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Subject $subject)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'coef' => 'required|numeric|min:0',
+        ]);
+
+        $subject->update($request->all());
+        return response()->json($subject);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Subject $subject
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Subject $subject)
     {
-        //
+        $subject->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
